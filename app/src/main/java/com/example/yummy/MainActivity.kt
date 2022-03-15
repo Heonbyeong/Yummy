deebc9965a2b51f3e3b9c373ffc6bb7b6ff7ecec
Anbyeonghun <abh0920one@gmail.com>
2022-03-15 23:27:10 +0900

    [Modify] showbar() 제거

--- app/src/main/java/com/example/yummy/MainActivity.kt
+++ app/src/main/java/com/example/yummy/MainActivity.kt
@@ -3,37 +3,19 @@ package com.example.yummy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import com.example.yummy.ui.theme.YummyTheme
-import com.example.yummy.ui.theme.primary
 import com.example.yummy.ui.theme.secondary
-import com.example.yummy.ui.theme.white
 import com.example.yummy.util.Navigation
-import com.example.yummy.util.ShowBar
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ShowBar(true)
             YummyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
