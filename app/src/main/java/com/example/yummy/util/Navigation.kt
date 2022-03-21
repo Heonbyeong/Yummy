1cd70a0f8b0a668ec47e84a7dc5b2274a6a4bc4c
Anbyeonghun <abh0920one@gmail.com>
2022-03-21 09:46:16 +0900

    [MODIFY] import 제거

--- app/src/main/java/com/example/yummy/util/Navigation.kt
+++ app/src/main/java/com/example/yummy/util/Navigation.kt
@@ -1,11 +1,6 @@
 package com.example.yummy.util
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
