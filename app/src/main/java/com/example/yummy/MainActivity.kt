1e6166f61660da333cc2419f02132292e8fdacc3
Anbyeonghun <abh0920one@gmail.com>
2022-03-26 16:47:19 +0900

    [Modify] Bottom nav 기능 구현 완료

--- app/src/main/java/com/example/yummy/MainActivity.kt
+++ app/src/main/java/com/example/yummy/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.yummy.ui.theme.YummyTheme
+import com.example.yummy.ui.theme.background
 import com.example.yummy.ui.theme.secondary
+import com.example.yummy.ui.theme.unSelectedColor
+import com.example.yummy.util.BottomNavContentScreens
+import com.example.yummy.util.BottomNavItem
 import com.example.yummy.util.Navigation
 
 class MainActivity : ComponentActivity() {
@@ -37,3 +51,63 @@ fun DefaultPreview() {
         //Greeting("Android")
     }
 }
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.screen_route) {
+        composable(BottomNavItem.Home.screen_route) {
+            BottomNavContentScreens().HomeScreen()
+        }
+        composable(BottomNavItem.Recommend.screen_route) {
+            BottomNavContentScreens().RecommendScreen()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Recommend
+    )
+    BottomNavigation(
+        backgroundColor = secondary,
+        contentColor = secondary
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title, fontSize = 10.sp) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = unSelectedColor,
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreenView() {
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { BottomNavigation(navController = navController) })
+    {
+        NavigationGraph(navController = navController)
+    }
+}
+
+
+
