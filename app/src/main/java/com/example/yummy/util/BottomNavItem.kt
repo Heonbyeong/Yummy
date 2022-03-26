eaef918b8060fbbf0ff1282b7878721724273e7e
Anbyeonghun <abh0920one@gmail.com>
2022-03-26 16:45:53 +0900

    [Modify] Navigation item class 추가

--- app/src/main/java/com/example/yummy/util/BottomNavItem.kt
+++ app/src/main/java/com/example/yummy/util/BottomNavItem.kt
@@ -3,6 +3,6 @@ package com.example.yummy.util
 import com.example.yummy.R
 
 sealed class BottomNavItem(var title: String, var icon: Int, var screen_route: String) {
-    object Home: BottomNavItem("Home", R.drawable.ic_home, "home")
-    object Recommend: BottomNavItem("Recommend", R.drawable.ic_recommend, "recommend")
+    object Home: BottomNavItem("홈", R.drawable.ic_home, "home")
+    object Recommend: BottomNavItem("추천", R.drawable.ic_recommend, "recommend")
 }
